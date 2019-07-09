4d86e23521212c81c684644e1896cf16950c9bc8
Bryce Eklund <43761688+bryceklund@users.noreply.github.com>
2019-07-09 15:31:15 -0700

    add API token validation

--- src/app.js
+++ src/app.js
@@ -13,6 +13,17 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path ${req.path}.`)
+        res.status(401).json({ error: 'Unauthorized request' })
+    };
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('HELL WORLD')
 });
