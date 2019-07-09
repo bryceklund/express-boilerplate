a88d580f005fa767552a77a902cd01338778a793
Bryce Eklund <43761688+bryceklund@users.noreply.github.com>
2019-07-09 15:36:42 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization');
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        logger.error(`Unauthorized request to path ${req.path}.`)
+        console.error(`Unauthorized request to path ${req.path}.`)
         res.status(401).json({ error: 'Unauthorized request' })
     };
     next();
